7bebff3ffc05f6300c0d030df7096d65ab815562
TiffyPox <tiffanyjaynedavies@hotmail.co.uk>
2024-01-29 21:12:42 -0500

    add variable_sized_arrays

---
+++ src/variable_sized_arrays.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+
+int main() 
+{
+    int numOfArrays = 0;
+    int numsOfQueries = 0;
+
+    std::cin >> numOfArrays >> numsOfQueries;
+    
+    std::cout << numOfArrays << " :: " << numsOfQueries << std::endl;
+
+    // to do: store the arrays
+    
+    return 0;
+}
