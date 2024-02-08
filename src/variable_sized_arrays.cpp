4dbfa4462b4faadc1bb8488d72c65e8a77ffc5dd
TiffyPox <tiffanyjaynedavies@hotmail.co.uk>
2024-02-07 21:20:56 -0500

    updated variable_sized_arrays

--- src/variable_sized_arrays.cpp
+++ src/variable_sized_arrays.cpp
@@ -7,14 +7,34 @@
 
 int main() 
 {
+    // get the ammount of arrays and queries
     int numOfArrays = 0;
     int numsOfQueries = 0;
 
     std::cin >> numOfArrays >> numsOfQueries;
-    
-    std::cout << numOfArrays << " :: " << numsOfQueries << std::endl;
 
-    // to do: store the arrays
-    
+    // create the arrays
+    int a[numOfArrays] = {0};
+
+    for (int n : a)
+    {
+        int arr[] = {};
+
+        // add numbers to temp array
+        for (int i = 0; i < 10; ++i)
+        {
+            std::cin >> arr[i];
+        }
+
+        int arrNum = 0;
+        int indexNum = 0;
+
+        // print final array
+        for (int i = 1; i < 10; ++i)
+        {
+
+        }
+    }
+
     return 0;
 }
