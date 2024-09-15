c39c112bf24ad787c16525704dd5eaa4620a9c06
tiffypox <tiffanypottenger@outlook.com>
2024-09-15 19:26:17 +0100

    update files

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+#include <iostream>
+
+int main() 
+{
+    return 0;
+}
---
+++ src/warmup_addition.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+int solveMeFirst(int a, int b) {
+    // Hint: Type return a+b; below:
+}
+
+int main() {
+    int num1 = 0; 
+    int num2 = 0;
+    int sum = 0;
+
+    std::cin >> num1 >> num2;
+
+    sum = solveMeFirst(num1,num2);
+
+    std::cout << sum;
+
+    return 0;
+}
