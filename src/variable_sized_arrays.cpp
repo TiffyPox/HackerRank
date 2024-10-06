2eb91e97a026c34f41256bfc452cf095b30594cc
TiffyPox <tiffanyjaynedavies@hotmail.co.uk>
2024-10-06 19:33:08 +0100

    update variable_sized_arrays

--- src/variable_sized_arrays.cpp
+++ src/variable_sized_arrays.cpp
@@ -1,19 +1,48 @@
-#include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
-#include <algorithm>
-#include <iterator>
 
 int main() 
 {
-    // get the amount of arrays and queries
-    int numOfArrays = 0;
-    int numsOfQueries = 0;
-
-    std::cin >> numOfArrays >> numsOfQueries;
-
-    // work in progress
-
+    int totalArrays = 0;
+    int totalQueries = 0;
+    
+    // Get number of arrays and number of queries:
+    std::cin >> totalArrays >> totalQueries;
+    
+    // Create a vector of vectors to hold the arrays:
+    std::vector<std::vector<int>> arrays(totalArrays);
+    
+    // Loop through each array:
+    for (int i = 0; i <totalArrays; ++i)
+    {
+        int totalNums = 0;
+        
+        // Get the number of elements in the current array:
+        std::cin >> totalNums;
+        
+        // Resize the current array to hold the elements:
+        arrays[i].resize(totalNums);
+        
+        // Read the elements into the current array:
+        for (int j = 0; j < totalNums; ++j)
+        {
+            std::cin >> arrays[i][j];
+        }
+    }
+    
+    // Process each query:
+    for (int result = 0; result < totalQueries; ++result)
+    {
+        int i = 0;
+        int j = 0;
+        
+        // Get the indices for the query:
+        std::cin >> i >> j;
+        
+        // Output the element at the specified indices:
+        std::cout << arrays[i][j] << std::endl;
+    }
+    
     return 0;
 }
